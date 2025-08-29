29a104875f92df308ff9b4221e9f146befd60519
Tsepaev Denis <snapz.2010@gmail.com>
2025-08-29 13:35:35 +0700

    Добавил тест на context_mune and alert

--- test_ui/test_herokuapp/test_ui_herokuapp.py
+++ test_ui/test_herokuapp/test_ui_herokuapp.py
@@ -3,6 +3,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.common.action_chains import ActionChains
 import pytest
 
 
@@ -14,7 +16,8 @@ URL = 'https://the-internet.herokuapp.com/'
     ("tomsmith", "SuperSecretPassword!", "You logged into a secure area!"),
     ("Tom", "JustPassword", "Your username is invalid")
 ])
-def test_log(user, password, message):
+def test_auth(user, password, message):
+    """Проверка авторизации пользователя"""
     driver = webdriver.Chrome()
     driver.get(URL + 'login')
     driver.maximize_window()
@@ -30,4 +33,21 @@ def test_log(user, password, message):
     else:
         mes = driver.find_element(By.CSS_SELECTOR, "#flash").text
         assert url_local == URL + "login" and mes.startswith(message)
+    driver.quit()
+
+
+
+
+def test_context_menu():
+    """Проверка вызова контекстного меню + alert"""
+    driver = webdriver.Chrome()
+    driver.get(URL + 'context_menu')
+    driver.maximize_window()
+    driver.implicitly_wait(5)
+    actions = ActionChains(driver)
+    actions.context_click(driver.find_element(By.CSS_SELECTOR, "#hot-spot")).perform()
+    mes = driver.switch_to.alert.text
+    driver.switch_to.alert.accept()
+    check = driver.find_element(By.CSS_SELECTOR, "a[target]").is_enabled()
+    assert mes == "You selected a context menu" and check
     driver.quit()
